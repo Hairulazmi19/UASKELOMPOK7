454ae688e766c3683ca931bd430f1a180b20b52d
Hairulazmi19 <hairuliyo@gmail.com>
2024-07-15 09:31:37 +0800

    Menambahkan Mahasiswa struct dan basic functions

--- mhs.cpp
+++ mhs.cpp
@@ -10,12 +10,4 @@ struct Mahasiswa {
     std::string jurusan;
     float nilai;
 };
-
-void clearScreen();
-void tambahMahasiswa(std::vector<Mahasiswa>& dataMahasiswa);
-void tampilkanMahasiswa(const std::vector<Mahasiswa>& dataMahasiswa);
-void hapusMahasiswa(std::vector<Mahasiswa>& dataMahasiswa, const std::string& NIM);
-void perbaruiMahasiswa(std::vector<Mahasiswa>& dataMahasiswa, const std::string& NIM);
-void cariMahasiswa(const std::vector<Mahasiswa>& dataMahasiswa);
-
 #endif // MAHASISWA_H
